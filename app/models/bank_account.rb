4a6be9a989ac128df97f4d016c257deede93e474
Catherine Jean <catherinejeanbean@gmail.com>
2018-02-28 15:07:55 -0600

    Add validations to bank account and to_s method

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,11 @@
 class BankAccount < ApplicationRecord
   belongs_to :client
+
+  validates :client, presence: true
+  validates :account_number, presence: true, uniqueness: true
+  validates :balance, presence: true, numericality: true
+
+  def to_s
+  	account_number
+  end
 end
