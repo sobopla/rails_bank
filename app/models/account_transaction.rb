476094e4beb1f8f28c07f03793f7676dd41c12cf
Catherine Jean <catherinejeanbean@gmail.com>
2018-02-28 15:18:48 -0600

    Give transactions a value of withdraw or deposit with a constant

--- app/models/account_transaction.rb
+++ app/models/account_transaction.rb
@@ -1,3 +1,9 @@
 class AccountTransaction < ApplicationRecord
   belongs_to :bank_account
+
+  TRANSACTION_TYPES = ["withdraw, deposit"] 
+
+  validates :bank_account, presence: true
+  validates :amount, presence: true, numericality: true
+  validates :transaction_type, presence: true, inclusion: { in: TRANSACTION_TYPES }
 end
