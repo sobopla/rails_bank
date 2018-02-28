b353e3e855b2a783fe31c814e9ae367f7d558eaa
Catherine Jean <catherinejeanbean@gmail.com>
2018-02-28 15:04:25 -0600

    Format client name with upcase /before_save

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,6 +4,14 @@ class Client < ApplicationRecord
   validates :middle_name, presence: true
   validates :client_number, presence: true, uniqueness: true
 
+  before_save :format_name
+
+  def format_name
+    self.first_name  = self.first_name.upcase
+    self.last_name   = self.first_name.upcase
+    self.middle_name = self.first_name.upcase
+  end
+
   def to_s
     "#{last_name}, #{first_name} #{middle_name}"
   end
