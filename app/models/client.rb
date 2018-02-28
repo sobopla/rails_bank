46a8bbb36c328703083971546429b9e3b3d95dee
Catherine Jean <catherinejeanbean@gmail.com>
2018-02-28 15:00:38 -0600

    Add validations to the client model and overwrite to_s method'

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,11 @@
 class Client < ApplicationRecord
+	validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :middle_name, presence: true
+  validates :client_number, presence: true, uniqueness: true
+
+  def to_s
+    "#{last_name}, #{first_name} #{middle_name}"
+  end
+
 end
